b5ed408d20f1ee4d692edecc088c7b922a78eb52
vinothkannans <vinothkannan@vinkas.com>
2016-05-22 15:11:55 +0530

    firebase integrated

--- app/src/main/java/com/ladyswings/mobile/MainActivity.java
+++ app/src/main/java/com/ladyswings/mobile/MainActivity.java
@@ -2,8 +2,16 @@ package com.ladyswings.mobile;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.vinkas.webkit.BrowserLayout;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +19,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HashMap<String, Object> defaults = new HashMap<>();
+        defaults.put("landing_page_url", "http://ladyswings.com/community/forum.php?styleid=135");
+        defaults.put("admob_banner_ad_unit_id", "");
+
+        FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
+        config.setDefaults(defaults);
+        config.fetch();
+        config.activateFetched();
+
+        String landing_page_url = config.getString("landing_page_url");
+        String ad_unit_id = config.getString("admob_banner_ad_unit_id");
+
         BrowserLayout browserLayout = (BrowserLayout) findViewById(R.id.browser);
-        browserLayout.loadUrl("https://ladyswings.com");
+        browserLayout.loadUrl(landing_page_url);
+
+        if (ad_unit_id != null && !ad_unit_id.isEmpty()) {
+            AdView adView = new AdView(getApplicationContext());
+            adView.setAdSize(AdSize.SMART_BANNER);
+            BrowserLayout.LayoutParams params = new BrowserLayout.LayoutParams(BrowserLayout.LayoutParams.MATCH_PARENT, BrowserLayout.LayoutParams.WRAP_CONTENT);
+            params.addRule(BrowserLayout.ALIGN_PARENT_BOTTOM);
+            params.addRule(BrowserLayout.CENTER_HORIZONTAL);
+            adView.setLayoutParams(params);
+            adView.setAdUnitId(ad_unit_id);
+            browserLayout.addView(adView);
+
+            AdRequest adRequest = new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .build();
+            adView.loadAd(adRequest);
+        }
     }
 
 }
--- module/src/main/java/com/vinkas/webkit/BrowserLayout.java
+++ module/src/main/java/com/vinkas/webkit/BrowserLayout.java
@@ -1,11 +1,13 @@
 package com.vinkas.webkit;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
-import android.widget.ProgressBar;
+import android.util.Log;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 /**
  * Created by Vinoth on 21-5-16.
@@ -22,6 +24,17 @@ public class BrowserLayout extends RelativeLayout implements SwipeRefreshLayout.
         initialize();
     }
 
+    public BrowserLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize();
+    }
+
+    @TargetApi(21)
+    public BrowserLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initialize();
+    }
+
     public void initialize() {
         swipeRefreshLayout = new SwipeRefreshLayout(getContext());
         swipeRefreshLayout.setLayoutParams(new BrowserLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
@@ -29,38 +42,47 @@ public class BrowserLayout extends RelativeLayout implements SwipeRefreshLayout.
         addView(swipeRefreshLayout);
 
         webView = new WebView(getContext());
-        webView.setLayoutParams(new SwipeRefreshLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        webView.setLayoutParams(new SwipeRefreshLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         webView.setOnPageLoadListener(this);
         swipeRefreshLayout.addView(webView);
     }
 
     @Override
     public void onFinished(WebView view, String url) {
-
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
     public void onStarted(WebView view, String url, Bitmap favicon) {
-
+        swipeRefreshLayout.setRefreshing(false);
+        swipeRefreshLayout.setRefreshing(true);
     }
 
     @Override
     public void onProgressChanged(WebView view, int newProgress) {
-        if(newProgress >= 100) {
+        if (newProgress >= 100)
             swipeRefreshLayout.setRefreshing(false);
-        } else {
+        else {
+            swipeRefreshLayout.setRefreshing(false);
             swipeRefreshLayout.setRefreshing(true);
         }
+        //Log.d(view.getUrl(), String.valueOf(newProgress));
     }
 
     private WebView webView;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private ScrollView scrollView;
+
+    public ScrollView getScrollView() {
+        return scrollView;
+    }
 
     public String getUrl() {
         return getWebView().getUrl();
     }
 
     public void loadUrl(String url) {
+        swipeRefreshLayout.setRefreshing(true);
         getWebView().loadUrl(url);
     }
 
